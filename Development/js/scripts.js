a4c23722456cbb8071bdc9d8a535d568907f01c3
noor-alsagheer <66689376+noor-alsagheer@users.noreply.github.com>
2020-06-27 20:55:49 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,27 +1,34 @@
 // السؤال الأول
 // 1
-
+const name = "noor";
 // 2
-
+let number = 16;
 // 3 and 4
-
+console. log( "انا" + name+" "+number +"عمري");
 // 5 and 6
+
+console.log( number*5);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hob = [ "drawing" , "reading" ];
+console.log(hob)
 // 2
-
+hob.push(16);
 // 3
 
 // 4
+let noooor= {track:"web",
 
+fav:"html",
+
+}
 // 5
 
 // 6
-
+console.log(noooor.track);
 // 7
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
